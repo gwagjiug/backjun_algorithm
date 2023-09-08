5f58ca84ed30f120df3df5c462d532f22333569b
gwagjiug <kwjo0228@naver.com>
2023-09-08 23:22:21 +0900

    2023 09 08 backjun

---
+++ src/Sort2.java
@@ -0,0 +1,35 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Scanner;
+
+public class Sort2 {
+    public static void main(String[] args) {
+
+
+        Scanner sc = new Scanner(System.in);
+        StringBuilder sb = new StringBuilder();
+
+        int N = sc.nextInt();
+
+        ArrayList<Integer> list = new ArrayList<>();
+
+        for(int i=0; i< N ;i++){
+
+            list.add(sc.nextInt());
+
+        }
+
+        Collections.sort(list);
+
+        for(int value : list){
+            sb.append(value).append('\n');
+        }
+
+        System.out.println(sb);
+
+
+
+
+    }
+}
