aea278e988c8ccd6a97f1d4eae94e4ca09e74153
gwagjiug <kwjo0228@naver.com>
2024-01-22 20:58:52 +0900

    2024 01 22

---
+++ src/Algorithm_3.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Algorithm_3 {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        long n = sc.nextInt();
+
+        System.out.println(n*n);
+        System.out.println(2);
+
+        sc.close();
+
+
+
+    }
+}
