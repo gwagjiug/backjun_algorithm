bca26b69a2635743016bcd0e22bc3c4f51b13533
gwagjiug <kwjo0228@naver.com>
2024-04-09 00:26:02 +0900

    2024-04-09

--- src/ATM.java
+++ src/ATM.java
@@ -11,6 +11,7 @@ public class ATM {
         int arr[] = new int[N];
 
         int sum = 0;
+        int prev = 0;
 
 
         for (int i =0; i<N;i++){
@@ -20,14 +21,23 @@ public class ATM {
 
         Arrays.sort(arr);
 
-        for (int i =0; i<N;i++){
+/*        for (int i =0; i<N;i++){
 
             for (int j =0; j<=i;j++){
                 sum += arr[j];
 
             }
 
+        }*/
+
+        for(int i = 0; i < N; i++) {
+            // 이전까지의 대기시간과 현재 사람이 걸리는 시간을 더해준다.
+            sum += prev + arr[i];
+
+            // 이전까지의 누적합에 현재 걸리는 시간을 더해준다.
+            prev += arr[i];
         }
+
 
         System.out.println(sum);
 
