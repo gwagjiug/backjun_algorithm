17794c36bdd38c99e4ce0d75f34b2f8f77e4b31f
gwagjiug <kwjo0228@naver.com>
2024-04-30 00:38:22 +0900

    2024-04-30

--- src/Sum_3.java
+++ src/Sum_3.java
@@ -28,6 +28,11 @@ public class Sum_3 {
             sum[i] = sum[i - 1] + Integer.parseInt(st.nextToken());
         }
 
+        //누적합 계산
+
+        /*구간의 합을 구할 때 누적합의 갑을 사용하여 시작 구간 이전의 누적합을 마지막 구간까지의 누적합에서 빼면 구간의 합을 구할 수 있음
+        * 3,5 구간의 합은  = 5까지의 누적합  - 3 이전의 누적합 */
+
         int M = Integer.parseInt(br.readLine());
         while (M-- > 0) {
             st = new StringTokenizer(br.readLine());
