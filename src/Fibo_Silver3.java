05e085dc5ad90be5c1f0010e69995662d1396fdb
gwagjiug <kwjo0228@naver.com>
2024-06-14 23:31:22 +0900

    2024-06-14
    백준 1003 dp연습

---
+++ src/Fibo_Silver3.java
@@ -0,0 +1,45 @@
+import java.util.Scanner;
+
+public class Fibo_Silver3 {
+
+    //백준 1003
+    static Integer[][ ]dp    = new Integer[41][2];
+    public static void main(String[] args) {
+
+
+        Scanner sc = new Scanner(System.in);
+
+        dp[0][0] = 1;	// N=0 일 때의 0 호출 횟수
+        dp[0][1] = 0;	// N=0 일 때의 1 호출 횟수
+        dp[1][0] = 0;	// N=1 일 때의 0 호출 횟수
+        dp[1][1] = 1;	// N=1 일 때의 1 호출 횟수
+
+        int T = sc.nextInt();
+
+        while(T --> 0){
+
+            int N = sc.nextInt();
+
+            fibonacci(N);
+            System.out.println(dp[N][0] + " " + dp[N][1]);
+
+
+        }
+
+
+    }
+
+    static Integer[] fibonacci(int N) {
+
+        if(dp[N][0] == null || dp[N][1] == null) {
+            // 각 N에 대한 0 호출 횟수와 1 호출 횟수를 재귀호출한다.
+            dp[N][0] = fibonacci(N - 1)[0] + fibonacci(N - 2)[0];
+            dp[N][1] = fibonacci(N - 1)[1] + fibonacci(N - 2)[1];
+        }
+        // N에 대한 0과 1, 즉 [N][0]과 [N][1] 을 담고있는 [N]을 반환한다.
+        return dp[N];
+
+    }
+
+
+}
