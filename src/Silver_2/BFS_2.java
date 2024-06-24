1144118768640e8b34864631be6f64823ef73ef2
gwagjiug <kwjo0228@naver.com>
2024-06-25 00:46:55 +0900

    2024-06-25
    백준 bfs2

---
+++ src/Silver_2/BFS_2.java
@@ -0,0 +1,84 @@
+package Silver_2;
+
+import java.io.*;
+import java.util.*;
+
+public class BFS_2 {
+    static int N,M,R;
+    static boolean [] visited;
+    static int [] order;
+    static ArrayList<Integer> [] graph;
+    static int count = 1;
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+
+        int R = Integer.parseInt(st.nextToken());
+
+        order = new int[N+1];
+        graph = new ArrayList[N+1];
+        visited = new boolean[N+1];
+
+
+        for(int i = 0; i<=N; i++){
+            graph[i] = new ArrayList<Integer>();
+        }
+
+        for(int i =0; i<M; i++){
+            st = new StringTokenizer(br.readLine());
+            int U = Integer.parseInt(st.nextToken());
+            int V = Integer.parseInt(st.nextToken());
+
+            graph[U].add(V);
+            graph[V].add(U);
+
+
+        }
+
+        for(int i = 0; i<=N; i++){
+            Collections.sort(graph[i],Collections.reverseOrder());
+        }
+
+
+            dfs(R);
+
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        for(int i = 1; i<=N; i++){
+            bw.write(order[i] + "\n");
+        }
+        bw.flush();
+        bw.close();
+
+    }
+
+    private static void dfs(int start) {
+        Queue<Integer> queue = new LinkedList<>();
+        queue.offer(start);
+
+
+        visited[start] = true;
+        order[start] = count++;
+
+        while(!queue.isEmpty()){
+            int x = queue.poll();
+
+            for(int i = 0; i<graph[x].size(); i++){
+                int y = graph[x].get(i);
+                if(!visited[y]){
+                    queue.offer(y);
+
+                    visited[y] = true;
+                    order[y] = count++;
+                }
+
+            }
+        }
+
+
+    }
+}
